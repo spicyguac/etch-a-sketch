d1ff816028ed506668422b2d2a5f83bb785e74d4
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2024-09-12 14:24:56 -0500

    Add class attribute to grid pieces

--- javascript.js
+++ javascript.js
@@ -2,5 +2,7 @@ const container = document.querySelector("div");
 
 for(i = 0; i < 15; i++){
     let gridPiece = document.createElement("div");
+    gridPiece.classList.add("gridPiece");
+    gridPiece.style.background = "gray";
     container.appendChild(gridPiece);
 }
