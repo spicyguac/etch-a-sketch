e626f89b0e78f0211fcc41629fbbfd4a063610a8
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2024-09-12 19:15:24 -0500

    Fix logic for making the grid
    Adjusted the size of the squares to fit the limits
    
    Fixed how many squares will be made to make an
    even square

--- javascript.js
+++ javascript.js
@@ -7,20 +7,26 @@ const userInput = document.querySelector("#getSquares");
 userInput.addEventListener("click", () => {
     numberOfSquares = prompt("How Many Squares Do You Want In Your Grid");
     numberOfSquares = +numberOfSquares;
+    createGrid(numberOfSquares);
 })
 
-for(i = 0; i < userinput; i++){
-    let gridPiece = document.createElement("div");
-    gridPiece.classList.add("gridPiece");
-    container.appendChild(gridPiece);
+function createGrid(userInput){
+    console.log(userInput**2);
+    for(i = 0; i < userInput**2; i++){
+        let gridPiece = document.createElement("div");
+        gridPiece.classList.add("gridPiece");
+        let dimensions = 960/userInput;
+        gridPiece.style.height = `${dimensions}px`;
+        gridPiece.style.width = `${dimensions}px`;
+        container.appendChild(gridPiece);
+    }
+    
+    const grid = document.querySelectorAll('.gridPiece');
+    
+    grid.forEach((gridPiece) => {
+        gridPiece.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = "black";
+        });
+    })
 }
 
-const grid = document.querySelectorAll('.gridPiece');
-
-console.log(grid.length);
-
-grid.forEach((gridPiece) => {
-    gridPiece.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = "black";
-    });
-})
